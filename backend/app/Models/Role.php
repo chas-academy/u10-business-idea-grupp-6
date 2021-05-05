31ea8cbeaf8d72a1ca6b82bee782d937ff12d64d
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-05-05 13:58:50 +0200

    adds a basic test-seeder, for trying out queries on the database

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -12,6 +12,6 @@ class Role extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class)->withTimestamps();
+        return $this->belongsToMany(User::class);
     }
 }
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Game;
+use App\Models\Genre;
+use App\Models\Lang;
+use App\Models\Misc;
+use App\Models\PlayerType;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,5 +30,51 @@ class DatabaseSeeder extends Seeder
         ])->roles()->attach(2);
 
         \App\Models\User::factory(10)->create();
+
+
+        //---------------------------------------TESTS
+            Genre::create(['genre' => 'action']);
+            Genre::create(['genre' => 'mmo']);
+
+            Game::create(['game' => 'Hitman 2', 'genre_id' => 1]);
+            Game::create(['game' => 'Habbo Hotel', 'genre_id' => 2]);
+
+            PlayerType::create(['type' => 'casual']);
+            PlayerType::create(['type' => 'competetive']);
+            PlayerType::create(['type' => 'medium']);
+
+            Lang::create(['lang_eng' => 'Swedish', 'native' => 'Svenska', 'code' => 'SE']);
+            Lang::create(['lang_eng' => 'Arabic', 'native' => 'عربى', 'code' => 'AR']);
+            Lang::create(['lang_eng' => 'English', 'native' => 'English', 'code' => 'ENG']);
+
+            Misc::create(['misc' => 'Non-aggressive language']);
+            Misc::create(['misc' => 'Flexible times']);
+
+            foreach(User::all() as $user)
+            {
+                $user->genres()->attach(rand(1, 2));
+                $user->games()->attach(rand(1, 2));
+                
+                for($i = 0; $i <= rand(0, 2); $i++)
+                {
+                    $user->player_types()->attach($i+1);
+                }
+
+                for($i = 0; $i <= rand(0, 2); $i++)
+                {
+                    $user->langs()->attach($i+1);
+                }
+
+                $num = rand(0, 2);
+                if($num) $user->miscs()->attach($num);
+
+            }
+
+
+
+
+
+
+        // ----------------------------------------TESTS
     }
 }
