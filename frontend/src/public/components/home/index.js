8b0da4ecd50c9c1751d1886ebc8d81dc1fb0e08c
EnzoBomark <enzo.bomark@chasacademy.se>
2021-05-05 18:01:45 +0200

    Init scss

---
+++ frontend/src/public/components/home/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './home.scss';
+
+export default function Home() {
+    return (
+        <>
+            <div className="grid">
+                <div>Vi</div>
+                <div>Älskar</div>
+                <div>Scss</div>
+                <div>,</div>
+                <div></div>
+                <div>Och</div>
+                <div>Grupp</div>
+                <div>6</div>
+                <div>Är</div>
+                <div>Bäst</div>
+            </div>
+        </>
+    )
+}
