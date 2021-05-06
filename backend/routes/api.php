760c18fc2651953690e696e041f9d75933c31aac
EnzoBomark <enzo.bomark@chasacademy.se>
2021-05-06 19:06:18 +0200

    Remove unused code

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,18 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function(Request $request) {
-    return $request->user();
-});
-
 require __DIR__ . '/json-api-auth.php';
-
-
 
 Route::group(['middleware' => ['auth:sanctum', 'verified']],  function () {
     Route::get('/user', function() {
         return response([
-            'message' => 'Already confirmed'
+            'message' => 'Confirmed'
         ]);
     });
 });
--- backend/routes/json-api-auth.php
+++ backend/routes/json-api-auth.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Actions\JsonApiAuth\AuthKit;
 use App\Http\Controllers\JsonApiAuth\ConfirmablePasswordController;
 use App\Http\Controllers\JsonApiAuth\EmailVerificationNotificationController;
 use App\Http\Controllers\JsonApiAuth\LoginController;
@@ -16,7 +15,7 @@ Route::post('/register', RegisterController::class)->name('json-api-auth.registe
 Route::post('/login', LoginController::class)->name('json-api-auth.login');
 
 Route::get('/logout', LogoutController::class)
-    ->middleware((AuthKit::getMiddleware()))
+    ->middleware(['auth:sanctum'])
     ->name('json-api-auth.logout');
 
 Route::post('/forgot-password', PasswordResetLinkController::class)
@@ -26,7 +25,7 @@ Route::post('/reset-password', NewPasswordController::class)
     ->name('json-api-auth.password.update');
 
 Route::post('/email/verification-notification', EmailVerificationNotificationController::class)
-    ->middleware([(AuthKit::getMiddleware()), 'throttle:6,1'])
+    ->middleware(['auth:sanctum', 'throttle:6,1'])
     ->name('json-api-auth.verification.send');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
@@ -34,5 +33,5 @@ Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
     ->name('json-api-auth.verification.verify');
 
 Route::post('/confirm-password', ConfirmablePasswordController::class)
-    ->middleware((AuthKit::getMiddleware()))
+    ->middleware(['auth:sanctum'])
     ->name('json-api-auth.password.confirm');
