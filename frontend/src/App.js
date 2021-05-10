5d055b8ede21ff3fd93099cd753e8e6d094f95ca
EnzoBomark <enzo.bomark@chasacademy.se>
2021-05-10 21:50:49 +0200

    Scss tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 // Components
 import Home from './components/home/'
+import Styles from './components/styles/'
 
 
 function App() {
   return (
       <Router>
             <Route path="/" exact component={Home} />
+            <Route path="/styles" exact component={Styles} />
       </Router>
   );
 }
--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -4,18 +4,7 @@ import './Home.scss';
 export default function Home() {
     return (
         <>
-            <div className="grid">
-                <div>Vi</div>
-                <div>Älskar</div>
-                <div>Scss</div>
-                <div>,</div>
-                <div></div>
-                <div>Och</div>
-                <div>Grupp</div>
-                <div>6</div>
-                <div>Är</div>
-                <div>Bäst</div>
-            </div>
+          <h1>Home works!</h1>
         </>
     )
 }
