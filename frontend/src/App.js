4bbc77ae076b144632f2a7476d78f6612311d4e2
EnzoBomark <enzo.bomark@chasacademy.se>
2021-05-11 10:07:31 +0200

    Fix scss structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-
-// Components
 import Home from './components/home/'
+import Styles from './components/styles/'
 
-
-function App() {
+const App = () => {
   return (
       <Router>
             <Route path="/" exact component={Home} />
+            <Route path="/styles" exact component={Styles} />
       </Router>
   );
 }
--- frontend/src/components/styles/StylesApi.jsx
+++ frontend/src/components/styles/StylesApi.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const HomeAPI = () => {
+const StylesApi = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const HomeAPI = () => {
     )
 }
 
-export default HomeAPI;
+export default StylesApi;
