3e57957ef400157e5aa67fccf091e2a9440f78a4
jamil-source <jamilbariche9@gmail.com>
2021-05-12 12:27:54 +0200

    fix password component

--- frontend/src/shared/components/input_password/index.jsx
+++ frontend/src/shared/components/input_password/index.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import './InputPassword.scss';
+import React, { useState } from "react";
+import eyeOpen from "../../assets/icons/eye-open.svg";
+import eyeClose from "../../assets/icons/eye-close.svg";
 
-const InputPassword = ({placeholder}) => {
-    return (
-        <>
-            <input type="password" className="input-password" placeholder={placeholder}/>
-        </>
-    )
-}
+import "./InputPassword.scss";
+
+const InputPassword = ({ placeholder }) => {
+  const [toggle, setToggle] = useState(true);
+
+  const toggleInputType = () => {
+    toggle ? setToggle(false) : setToggle(true);
+  };
+
+  return (
+    <>
+      <div className="input-password">
+        <input className="input-text" type={toggle ? "password" : "text"} placeholder={placeholder} />
+        <input id="toggle-pw" type="checkbox" onChange={toggleInputType} />
+        <label className="toggle-label" htmlFor="toggle-pw"/>
+      </div>
+    </>
+  );
+};
 
 export default InputPassword;
+
+{/* <img src={toggle ? eyeOpen : eyeClose} width="40" alt="" /> */}
