04843eb42b3950169908b97fea301b3fb337c64f
EnzoBomark <enzo.bomark@chasacademy.se>
2021-05-13 09:19:35 +0200

    Update RegisterAPI.jsx

--- frontend/src/components/register/RegisterAPI.jsx
+++ frontend/src/components/register/RegisterAPI.jsx
@@ -2,12 +2,14 @@ import axios from 'axios'
 const apiBaseURL = 'BACKEND_URL'
 
 export const GET = (url) => {
-    return axios.get(`${apiBaseURL}/${url}`);
+    // return axios.get(`${apiBaseURL}/${url}`);
+    return console.log(url);
 }
 
 export const POST = (url, data) => {
-    return axios(`${apiBaseURL}/${url}`, {
-        method: 'POST',
-        data,
-    });
+    // return axios(`${apiBaseURL}/${url}`, {
+    //     method: 'POST',
+    //     data,
+    // });
+    return console.log(url, data);
 }
