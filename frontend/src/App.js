8d3626d3a62d82a29bd84ada2396a4be88d7b30b
Karin Stenwall <karin.stenwall@gmail.com>
2021-05-13 12:07:50 +0200

    add: route for succesfully verified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,21 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/home/'
 import Register from './components/register/'
 import Login from './components/login/'
+import VerifiedSuccesfully from './components/verified_succesfully';
+import VerifiedAlready from './components/verified_already';
 
 const App = () => {
     return (
-        <>
-            <main>
-                <Router>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/register" exact component={Register} />
-                    <Route path="/login" exact component={Login} />
-                </Router>
-            </main>
-        </>
+      <>
+        <main>
+          <Router>
+            <Route path="/" exact component={Home} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/verified_succesfully" exact component={VerifiedSuccesfully} />
+          </Router>
+        </main>
+      </>
     );
 }
 
