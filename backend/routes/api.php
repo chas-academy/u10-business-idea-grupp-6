92b00b542b0f006248882c3818172c8b768f7df7
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-05-13 14:20:24 +0200

    Remove CORS headers from api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
+// header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
+// header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
 
 Route::post('/test', [MatchController::class, 'test']);
 require __DIR__ . '/json-api-auth.php';
