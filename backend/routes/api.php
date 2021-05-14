54ac5410c6c594f899648085ee5540952c9ddc52
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-05-14 13:19:06 +0200

    Unused controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\MatchController;
 use App\Http\Controllers\PreferenceController;
-use App\Http\Controllers\Preferences\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
