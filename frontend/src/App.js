464fdf3146118bd7c03ad6402c7f73fb46699ed6
EnzoBomark <enzo.bomark@chasacademy.se>
2021-05-18 15:19:13 +0200

    Remove test code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-import Home from './components/home/'
-import Styles from './components/styles/'
 
 const App = () => {
     return (
         <>
             <main>
                 <Router>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/styles" exact component={Styles} />
                 </Router>
             </main>
         </>
