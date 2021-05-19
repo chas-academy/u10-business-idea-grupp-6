1175dcb29cfe347fce6b1620d566bc13ac7d4b41
Karin Stenwall <karin.stenwall@gmail.com>
2021-05-19 12:07:29 +0200

    add: input fields + styling

---
+++ frontend/src/components/edit-profile/index.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./EditProfile.scss";
+import { Link } from "react-router-dom";
+import {
+  Input,
+  ButtonSubmit,
+  MessageError,
+} from "../../shared/components/";
+import { POST } from "../../shared/services/requests";
+
+const EditProfile = () => {
+  const [displayName, setDisplayName] = useState(''),
+        [country, setCountry] = useState('');
+        // [imgPath, setImgPath] = useState(''),
+        // [body, setBody] = useState(''),
+
+  const getDisplayName = (e) => setDisplayName(e),
+        getCountry = (e) => setCountry(e);
+        // getImgPath = (e) => setImgPath(e),
+        // getBody = (e) => setBody(e);
+
+  const submit = (event) => {
+    console.log(event.target[0].value);
+    event.preventDefault();
+    const data = {
+      display_name: displayName,
+      country: country,
+      // img_path: imgPath,
+      // body: body,
+    };
+
+    POST("/user/{user}", data)
+      .then((data) => {
+        console.log(data);
+        // localStorage.setItem("token", data.data.token);
+      })
+      .catch((error) => {
+        console.log(error);
+        // setError(error.response.data.message);
+      });
+  };
+
+  return (
+    <>
+      <h1 className="profile-title">Edit Public Profile</h1>
+
+      <form className="profile-form" onSubmit={submit}>
+        {/* {errorEmail && <MessageError message = {errorEmail}/>} */}
+
+        <Input
+          type="text"
+          placeholder="Display Name"
+          name="display_name"
+          getState={getDisplayName}
+        />
+
+        <Input
+          type="text"
+          placeholder="Country"
+          name="country"
+          getState={getCountry}
+        />
+
+        <ButtonSubmit name="Profile" />
+      </form>
+    </>
+  );
+};
+
+export default EditProfile;
