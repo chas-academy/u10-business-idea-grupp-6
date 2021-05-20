40b7b02e226e6980630d329e7e4b8e9200eedd87
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-05-20 15:47:07 +0200

    removed local url

--- frontend/src/shared/services/requests.js
+++ frontend/src/shared/services/requests.js
@@ -4,8 +4,7 @@ import Pusher from 'pusher-js';
 
 window.Pusher = require('pusher-js');
 
-// const apiBaseURL = 'https://u10-backend-staging.herokuapp.com/api'
-const apiBaseURL = 'http://u10.test/api'
+const apiBaseURL = 'https://u10-backend-staging.herokuapp.com/api'
 
 axios.defaults.withCredentials = true;
 
