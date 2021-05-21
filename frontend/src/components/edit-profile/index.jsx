c9865d6a3b7797ab9843548f3c81c677638d6ddb
Karin Stenwall <karin.stenwall@gmail.com>
2021-05-21 09:54:26 +0200

    add: patch and error handling

--- frontend/src/components/edit-profile/index.jsx
+++ frontend/src/components/edit-profile/index.jsx
@@ -11,15 +11,16 @@ import {
 import { PATCH } from "../../shared/services/requests";
 
 const EditProfile = () => {
-  const [displayName, setDisplayName] = useState(''),
-        [country, setCountry] = useState(''),
-        // [imgPath, setImgPath] = useState(''),
-        [body, setBody] = useState('');
+  const [displayName, setDisplayName] = useState(""),
+    [country, setCountry] = useState(""),
+    // [imgPath, setImgPath] = useState(''),
+    [body, setBody] = useState(""),
+    [errorDisplayName, setErrorDisplayName] = useState(null);
 
   const getDisplayName = (e) => setDisplayName(e),
-        getCountry = (e) => setCountry(e),
-        // getImgPath = (e) => setImgPath(e),
-        getBody = (e) => setBody(e);
+    getCountry = (e) => setCountry(e),
+    // getImgPath = (e) => setImgPath(e),
+    getBody = (e) => setBody(e);
 
   const submit = (event) => {
     console.log(event.target[0].value);
@@ -31,24 +32,28 @@ const EditProfile = () => {
       body: body,
     };
 
-    PATCH("/user/{user}", data)
+    const userId = localStorage.getItem("user_id");
+
+    PATCH(`user/${userId}`, data)
       .then((data) => {
         console.log(data);
-        // localStorage.setItem("token", data.data.token);
       })
       .catch((error) => {
         console.log(error);
-        // setError(error.response.data.message);
+        setErrorDisplayName(error.response.data.error.display_name);
       });
   };
 
   return (
     <>
       <h1 className="profile-title">Edit Profile</h1>
-      <h2 className="profile-sub-title">This is your public profile that other people can see</h2>
+      <h2 className="profile-sub-title">
+        This is your public profile that other people can see
+      </h2>
 
       <form className="profile-form" onSubmit={submit}>
-        {/* {errorEmail && <MessageError message = {errorEmail}/>} */}
+  
+        {errorDisplayName && <MessageError message={errorDisplayName} />}
 
         <Input
           type="text"
@@ -57,14 +62,14 @@ const EditProfile = () => {
           getState={getDisplayName}
         />
 
-        <InputDropdown />
+        {/* <InputDropdown />
 
         <Input
           type="text"
           placeholder="Country"
           name="country"
           getState={getCountry}
-        />
+        /> */}
 
         <Textarea
           name="body"
