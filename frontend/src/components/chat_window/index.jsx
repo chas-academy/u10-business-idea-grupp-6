f9b09203d958f8f54b55d9db0a6d0d1121185b72
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-05-21 11:40:08 +0200

    fixes polarized bug in frontend

--- frontend/src/components/chat_window/index.jsx
+++ frontend/src/components/chat_window/index.jsx
@@ -22,9 +22,9 @@ const ChatWindow = ({ active, matchup }) => {
             console.log('subscribing to session chat ' + matchup.session.id)
             
             echo.private(`Chat.${matchup.session.id}`).listen('PrivateChatEvent', (e) => {
-                console.log(e)
+                // console.log(e)
                 const f = e;
-                if (f.chat.user_id !== parseInt(localStorage.getItem('user_id')))
+                if (f.chat.user_id === parseInt(localStorage.getItem('user_id')))
                 {
                     f.chat.type = 1;
                 }
