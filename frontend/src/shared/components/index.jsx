de6deec28afd3629ff248b31190ffb3c0022da25
Karin <66753700+stenwall@users.noreply.github.com>
2021-05-24 15:13:49 +0200

    (redoing changes)

--- frontend/src/shared/components/index.jsx
+++ frontend/src/shared/components/index.jsx
@@ -5,7 +5,6 @@ import InputDropdown from './input_dropdown/';
 import InputPassword from './input_password/';
 import Slider from './slider/';
 import MessageError from './message_error/';
-import Modal from './modal/';
 import Switch from './switch/';
 import Textarea from './textarea/';
 
@@ -18,6 +17,5 @@ export {
   Slider,
   Switch,
   MessageError,
-  Modal,
   Textarea,
 };
