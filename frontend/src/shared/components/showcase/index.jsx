4fd1d38cf8bd37fa7b2a702d9290eb7c47ada722
EnzoBomark <enzo.bomark@chasacademy.se>
2021-05-25 08:52:14 +0200

    Add pagination

--- frontend/src/shared/components/showcase/index.jsx
+++ frontend/src/shared/components/showcase/index.jsx
@@ -3,22 +3,51 @@ import './Showcase.scss';
 
 const Showcase = ({ data, type }) => {
   const [items, setItems] = useState(),
-        [position, setPosition] = useState(0)
-
+        [adder, setAdder] = useState(0),
+        [position, setPosition] = useState({
+          min: 0, 
+          max: 2,
+        })
+  
   useEffect(() => {
-
-    const filtered = data.filter((i, index) => index >= position && index < position + 3);
+    const filtered = data
+      .filter((i, index) => index >= position.min && index < position.max + adder);
 
     setItems(filtered);
+  }, [position])
+
+  useEffect(() => {
+    window.addEventListener('resize', event => {
+      if(window.innerWidth <= 380) return console.log('0');
+      if(window.innerWidth <= 768) return console.log('1');
+      if(window.innerWidth <= 1365) return console.log('2');
+      if(window.innerWidth > 1366) return console.log('3');
+    });
   }, [])
+
+  const left = () => {
+    if(position.min > 0) setPosition(prevState => ({
+      ...prevState,
+      min: position.min--,
+      max: position.max--
+    }));
+  }
+
+  const right = () => {
+    if(position.max < data.length) setPosition(prevState => ({
+      ...prevState,
+      min: position.min++,
+      max: position.max++
+    }));
+  }
 
   return (
     <div className="showcase" >
-      <button>&#9664;</button>
+      <button onClick={left}>&#9664;</button>
       {items?.map(item =>
         <div className="item">{item[type]}</div>
       )}
-      <button>&#9654;</button>
+      <button onClick={right}>&#9654;</button>
     </div>
   )
 }
