92a3ce324c5e0246c1f0a479c93ca50ab3e2044f
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-05-26 12:08:57 +0200

    Visual bugfixes, and link from verified
    Co-authored-by: hannesqvarnstrom <hannesqvarnstrom@users.noreply.github.com>

--- frontend/src/components/chat/index.jsx
+++ frontend/src/components/chat/index.jsx
@@ -67,74 +67,92 @@ const Chat = () => {
     }
 
     return (
-        <>
-            <h1 className="chat-title">
-                Chat Room
-            </h1>
+      <>
+        <h1 className="chat-title">Chat Room</h1>
 
-      {matchups.length === 0 && <div>You have no more matches...
-                <ButtonLink
-
-          name="Match"
-          classValue="button-link" />
-
-      </div>}
-
-      {matchups.map(matchup =>
-        matchup &&
-        <div 
-        className="chat-box"
-        onClick={() => handleSetActiveChat(matchup.id, matchup.session, matchup.user[0].id)}
-        >
-          <div
-          >
-            <img
-              src="https://image.flaticon.com/icons/png/512/1077/1077114.png"
-              width="30px"
-              className="profile-img"
-            />
-            <p className="chat-displayname">
-              {matchup.user[0].profile.display_name}
-            </p>
-            {/* write user info here */}
+        {matchups.length === 0 && (
+          <div>
+            You have no more matches...
+            <ButtonLink name="Match" classValue="button-link" />
           </div>
-          <span 
-          className="chat-options-wrapper"
-          >
-            <span
-              className="chat-options"
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                setOptionModal((previousState) => previousState === matchup.id ? null : matchup.id)
-              }}>
-                <FontAwesomeIcon icon={faEllipsisV} />
-              </span>
+        )}
 
-              <div 
-              className={`chat-options-modal ${optionModal === matchup.id && "shown"}`}
+        {matchups.map(
+          (matchup) =>
+            matchup && (
+              <div
+                className="chat-box"
+                onClick={() =>
+                  handleSetActiveChat(
+                    matchup.id,
+                    matchup.session,
+                    matchup.user[0].id
+                  )
+                }
               >
-                <span
-                  className="un-match"
-                  onClick={() => { handleUnmatch(matchup.id) }}
+                <img
+                  src="https://image.flaticon.com/icons/png/512/1077/1077114.png"
+                  width="30px"
+                  className="profile-img"
+                />
+                <p
+                  className="chat-displayname"
+                  aria-label={matchup.user[0].profile.display_name || "User hasn't selected a display name"}
+                  title={matchup.user[0].profile.display_name || "User hasn't selected a display name"}
                 >
-                  Un-match
+                  {matchup.user[0].profile.display_name?.length > 17
+                    ? matchup.user[0].profile.display_name.substr(0, 17) + "..."
+                    : matchup.user[0].profile.display_name}
+                </p>
+                {/* write user info here */}
+
+                <span className="chat-options-wrapper">
+                  <span
+                    className="chat-options"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      setOptionModal((previousState) =>
+                        previousState === matchup.id ? null : matchup.id
+                      );
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faEllipsisV} />
+                  </span>
+
+                  <div
+                    className={`chat-options-modal ${
+                      optionModal === matchup.id && "shown"
+                    }`}
+                  >
+                    <span
+                      className="un-match"
+                      onClick={() => {
+                        handleUnmatch(matchup.id);
+                      }}
+                    >
+                      Un-match
+                    </span>
+                  </div>
                 </span>
               </div>
-            </span>
-          </div>
-      )}
-      {matchups.map((matchup) =>
-        <ChatWindow
-          active={(activeChat && matchup.session) && activeChat === matchup.session.id ? true : false}
-          matchup={matchup}
-          closeChat={handleCloseChat}
-          openChat={handleOpenChat}
-          key={matchup.user[0].id}
-        />
-      )}
-    </>
-  )
+            )
+        )}
+        {matchups.map((matchup) => (
+          <ChatWindow
+            active={
+              activeChat && matchup.session && activeChat === matchup.session.id
+                ? true
+                : false
+            }
+            matchup={matchup}
+            closeChat={handleCloseChat}
+            openChat={handleOpenChat}
+            key={matchup.user[0].id}
+          />
+        ))}
+      </>
+    );
 }
 
 export default Chat
--- frontend/src/components/chat_window/index.jsx
+++ frontend/src/components/chat_window/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { echo, POST } from '../../shared/services/requests';
 import './ChatWindow.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 const ChatWindow = ({ active, matchup, closeChat, openChat }) => {
 
     const [inputValue, setInputValue] = useState(""),
@@ -50,64 +51,52 @@ const ChatWindow = ({ active, matchup, closeChat, openChat }) => {
         closeChat();
     }
 
-    return <>
-        {active &&
-            <div class="chat-modal">
-                 <img
-                    src="https://image.flaticon.com/icons/png/512/860/860790.png"
-                    width="40px"
-                    className="back-arrow"
-                    onClick={toggleChat}
-                />
-                <h1 className="chatwindow-title">
-                    {matchup.user[0].profile.display_name}
-                </h1>
-               
+    return (
+      <>
+        {active && (
+          <div class="chat-modal">
+            <FontAwesomeIcon
+              icon={faAngleLeft}
+              className="back-arrow"
+              onClick={toggleChat}
+            />
+            <h3 className="chatwindow-title">
+              {matchup.user[0].profile.display_name}
+            </h3>
 
-                <div id="chatbox" className="chatbox">
-                    {messageLog.map(i => 
-                        <div className={parseInt(i.type) ?  "received" : "sent"}>
-                            <div className="chatbox-bubble">
-                                <p key={i.id}
-                                >
-                                {i.content}
-                                </p>
-                                <p className="chatbox-sent">
-                                {i.send_at}
-                                </p>
-                        </div>
+            <div id="chatbox" className="chatbox">
+              {messageLog.map((i) => (
+                <div className={parseInt(i.type) ? "received" : "sent"}>
+                  <div className="chatbox-bubble">
+                    <p key={i.id}>{i.content}</p>
+                    <p className="chatbox-sent">{i.send_at}</p>
+                  </div>
                 </div>
-                
-
-                )}
-                {newMessages.map(i => 
-                    
-                        <p key={i.id}
-                        className={i.chat.type ? "received" : "sent"}
-                        >
-                        {i.content}
-                        </p>
-                    
-                    )}
-            
-                    </div>
-                    <div className="chatwindow">
-                        <form onSubmit={submit}>
-
-                            <input 
-                                type="text" 
-                                name="message" 
-                                onChange={(e) => setInputValue(e.target.value)} 
-                                className="chatwindow-textarea"
-                                value={inputValue}
-                                placeholder={"Message " + matchup.user[0].profile.display_name}
-                            />
-                    </form>
-                </div>
+              ))}
+              {newMessages.map((i) => (
+                <p key={i.id} className={i.chat.type ? "received" : "sent"}>
+                  {i.content}
+                </p>
+              ))}
             </div>
-        }
-
-    </>
+            <div className="chatwindow">
+              <form onSubmit={submit}>
+                <input
+                  type="text"
+                  name="message"
+                  onChange={(e) => setInputValue(e.target.value)}
+                  className="chatwindow-textarea"
+                  value={inputValue}
+                  placeholder={
+                    "Message " + matchup.user[0].profile.display_name
+                  }
+                />
+              </form>
+            </div>
+          </div>
+        )}
+      </>
+    );
 }
 
 export default ChatWindow
