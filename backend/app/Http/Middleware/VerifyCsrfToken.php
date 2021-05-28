56f9bdd68a2fab7758c4231027904943fc2dc594
Simon Lindelöf <70695363+reelode@users.noreply.github.com>
2021-05-28 17:17:12 +0200

    Update VerifyCsrfToken.php

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/api/*',
-        'nova/login'
+        'nova/login',
+        'nova/dashboards/main'
     ];
 }
