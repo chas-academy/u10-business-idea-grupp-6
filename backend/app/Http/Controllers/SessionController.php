13834aba4a790f5c2c449137db071ca0d16dbf17
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-05-31 11:22:01 +0200

    adds logic for session creation

--- backend/app/Http/Controllers/SessionController.php
+++ backend/app/Http/Controllers/SessionController.php
@@ -11,8 +11,16 @@ class SessionController extends Controller
 {
     public function create(Request $request)
     {
-        $session = Session::create(['user_a_id' => auth('sanctum')->user()->id, 'user_b_id' => $request->friend_id]);
-        $modifiedSession = new SessionResource($session);
+        if($session = Session::where('user_a_id', $request->friend_id)->where('user_b_id',  auth('sanctum')->user()->id))
+        {
+            $modifiedSession = new SessionResource($session);
+        }
+        else 
+        {
+            $session = Session::create(['user_a_id' => auth('sanctum')->user()->id, 'user_b_id' => $request->friend_id]);
+            $modifiedSession = new SessionResource($session);
+        }
+
         broadcast(new SessionEvent($modifiedSession, auth('sanctum')->user()->id));
 
         return $modifiedSession;
