fde02d890becac442709ee5c5450b9aa736a142c
hannesqvarnstrom <70695417+hannesqvarnstrom@users.noreply.github.com>
2021-05-31 09:51:27 +0200

    Update requests.js

--- frontend/src/shared/services/requests.js
+++ frontend/src/shared/services/requests.js
@@ -5,7 +5,7 @@ import Pusher from 'pusher-js';
 window.Pusher = require('pusher-js');
 
 // const apiBaseURL = 'https://u10-backend-staging.herokuapp.com/api';
-const apiBaseURL = 'http://tenta.designproducers.se/api';
+const apiBaseURL = 'https://tenta.designproducers.se/api';
 
 axios.defaults.withCredentials = true;
 
