773ee1e29eb9fac4f225b04072b79aa55e652978
Karin Stenwall <karin.stenwall@gmail.com>
2021-06-02 10:05:10 +0200

    add: head component (w helmet)

---
+++ frontend/src/components/head/index.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Home = () => {
+  return (
+    <Helmet
+
+    {/*
+        (optional) Useful when you want titles to inherit from a template:
+
+        <Helmet
+            titleTemplate="%s | MyAwesomeWebsite.com"
+        >
+            <title>Nested Title</title>
+        </Helmet>
+
+        outputs:
+
+        <head>
+            <title>Nested Title | MyAwesomeWebsite.com</title>
+        </head>
+    */}
+    titleTemplate="MySite.com - %s"
+
+    {/*
+        (optional) used as a fallback when a template exists but a title is not defined
+
+        <Helmet
+            defaultTitle="My Site"
+            titleTemplate="My Site - %s"
+        />
+
+        outputs:
+
+        <head>
+            <title>My Site</title>
+        </head>
+    */}
+    defaultTitle="My Default Title"
+    
+    {/* (optional) set to false to not use requestAnimationFrame and instead update the DOM as soon as possible.
+        Useful if you want to update the title when the tab is out of focus 
+    */}
+    defer={false}
+
+    {/* (optional) callback that tracks DOM changes */}
+    onChangeClientState={(newState, addedTags, removedTags) => console.log(newState, addedTags, removedTags)}
+>
+    {/* html attributes */}
+    <html lang="en" amp />
+
+    {/* body attributes */}
+    <body className="root" />
+
+    {/* title attributes and value */}
+    <title itemProp="name" lang="en">My Plain Title or {`dynamic`} title</title>
+
+    {/* base element */}
+    <base target="_blank" href="http://mysite.com/" />
+
+    {/* multiple meta elements */}
+    <meta name="description" content="Helmet application" />
+    <meta property="og:type" content="article" />
+
+    {/* multiple link elements */}
+    <link rel="canonical" href="http://mysite.com/example" />
+    <link rel="apple-touch-icon" href="http://mysite.com/img/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="http://mysite.com/img/apple-touch-icon-72x72.png" />
+    {locales.map((locale) => {
+        <link rel="alternate" href="http://example.com/{locale}" hrefLang={locale} key={locale}/>
+    })}
+
+    {/* multiple script elements */}
+    <script src="http://include.com/pathtojs.js" type="text/javascript" />
+
+    {/* inline script elements */}
+    <script type="application/ld+json">{`
+        {
+            "@context": "http://schema.org"
+        }
+    `}</script>
+
+    {/* noscript elements */}
+    <noscript>{`
+        <link rel="stylesheet" type="text/css" href="foo.css" />
+    `}</noscript>
+
+    {/* inline style elements */}
+    <style type="text/css">{`
+        body {
+            background-color: blue;
+        }
+
+        p {
+            font-size: 12px;
+        }
+    `}</style>
+</Helmet>
+  );
+};
+
+export default Home;
